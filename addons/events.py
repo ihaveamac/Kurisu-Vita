6ec9e3b17ebd9a3722c84e184a54350662647095
SCOTT0852 <33875157+SCOTT0852@users.noreply.github.com>
2019-06-16 19:57:39 -0700

    I'm an idiot

--- addons/events.py
+++ addons/events.py
@@ -51,19 +51,19 @@ class Events:
         'fr$shop',
         'pkgi',
         'pkgj',
-        'pkjl'
-        'pckgj'
-        'pckgl'
-        'pckgi'
-        'rvitapiracy'
-        'r/vitapiracy'
+        'pkjl',
+        'pckgj',
+        'pckgl',
+        'pckgi',
+        'rvitapiracy',
+        'r/vitapiracy',
         'npsbrowser',
-        'npsstore'
-        'npstore'
-        'npsshop'
-        'npshop'
-        'yespaystation'
-        'nopaystation'
+        'npsstore',
+        'npstore',
+        'npsshop',
+        'npshop',
+        'yespaystation',
+        'nopaystation',
         'titlekeys',
     ]
 
